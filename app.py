681e3c7a4d1478427485d4c815c1825bd176dd2e
pinpingpong <81296774+pinpingpong@users.noreply.github.com>
2022-03-16 22:29:16 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[46]:
+
+
+from flask import Flask, request, render_template
+import joblib
+
+
+# In[47]:
+
+
+app = Flask(__name__)
+
+
+# In[48]:
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST": 
+        income = request.form.get("Income")
+        print(income)
+        model1 = joblib.load("FOOD_REG")
+        pred1 = model1.predict([[income]])
+        str1 = "The prediction for your Food Expenditure based on your income using Regression is : " + str(pred1[0][0])
+        return(render_template("index.html", result1=str1))
+    else:
+        return(render_template("index.html", result1="Your result will be shown here"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__": 
+    app.run()
+
+
+# In[ ]:
+
+
+
+
